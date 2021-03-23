6206e843575383581be36af10c35efe8537ed5aa
Anirudh C <anirudh.c@iiitb.org>
2021-03-24 01:00:20 +0530

    add: simple landing page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
-const App = () => (
-  <h1>FPL Assistant</h1>
-);
+const useStyles = makeStyles((theme) => ({
+    root: {
+        height: '100vh',
+    },
+    image: {
+        backgroundImage: 'url(https://www.soccerbible.com/media/117377/nike-ball-2-min.jpg)',
+        backgroundRepeat: 'no-repeat',
+        backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+    },
+    paper: {
+        margin: theme.spacing(8, 4),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    loader: {
+        margin: theme.spacing(12, 0)
+    }
+}));
+
+function App() {
+    const classes = useStyles();
+
+    return (
+        <Grid container component="main" className={classes.root}>
+          <CssBaseline />
+          <Grid item xs={false} sm={4} md={7} className={classes.image} />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+            <div className={classes.paper}>
+              <Typography component="h1" variant="h4">
+                Fantasy Premier League
+              </Typography>
+              <Typography>
+                The Assistant
+              </Typography>
+              <CircularProgress className={classes.loader}/>
+            </div>
+          </Grid>
+        </Grid>
+    );
+}
 
 export default App;
