8581c929d9fed3da585e52288e1ef3088b059781
anirudh-c <anirudh.c@iiitb.org>
2021-03-29 01:54:55 +0530

    add: login page and router code

--- src/App.js
+++ src/App.js
@@ -1,54 +1,36 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
-import CircularProgress from '@material-ui/core/CircularProgress';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        height: '100vh',
-    },
-    image: {
-        backgroundImage: 'url(https://www.soccerbible.com/media/117377/nike-ball-2-min.jpg)',
-        backgroundRepeat: 'no-repeat',
-        backgroundColor:
-        theme.palette.type === 'light' ? theme.palette.grey[50] : theme.palette.grey[900],
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-    },
-    paper: {
-        margin: theme.spacing(8, 4),
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    loader: {
-        margin: theme.spacing(12, 0)
+import Login from "./Login";
+import Home from "./Home";
+
+class App extends React.Component {
+    handleLogin(login, password) {
+        console.log(login);
+        console.log(password);
     }
-}));
 
-function App() {
-    const classes = useStyles();
-
-    return (
-        <Grid container component="main" className={classes.root}>
-          <CssBaseline />
-          <Grid item xs={false} sm={4} md={7} className={classes.image} />
-          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
-            <div className={classes.paper}>
-              <Typography component="h1" variant="h4">
-                Fantasy Premier League
-              </Typography>
-              <Typography>
-                The Assistant
-              </Typography>
-              <CircularProgress className={classes.loader}/>
-            </div>
-          </Grid>
-        </Grid>
-    );
+    render() {
+        return (
+            <Router>
+              <div>
+                <Switch>
+                  <Route path="/login">
+                    <Login loginCallback={this.handleLogin}/>
+                  </Route>
+                  <Route path="/">
+                    <Home />
+                  </Route>
+                </Switch>
+              </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
