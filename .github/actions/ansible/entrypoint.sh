8c45f7b44c96cd937e89fe829ecdfa4cd01f0520
Anirudh C <anirudh.c@iiitb.org>
2021-04-17 21:42:11 +0530

    add: ansible GitHub action

---
+++ .github/actions/ansible/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "$GCLOUD_SERVICE_ACCOUNT_KEY" > ~/.service_account_key.json
+gcloud auth activate-service-account "$GCLOUD_SERVICE_ACCOUNT" --key-file ~/.service_account_key.json
+
+echo "$VAULT_PASS" > ~/.vault_pass.txt
+ansible-playbook --vault-password-file ~/.vault_pass.txt -i "$INVENTORY" "$PLAYBOOK"
